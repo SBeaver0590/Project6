776a4fd954b6bbfbf966311341a387d88676e121
Simone Beaver <simone.beaver@verizon.com>
2019-08-02 09:53:22 -0500

    working code

--- app.js
+++ app.js
@@ -4,27 +4,27 @@ const pug = require('pug');
 const data= require('./data.json');
 const path= require('path');
 
-app.set('views', path.join(__dirname, 'views'));
+
+//Set the view engine to pug
 app.set('view engine', 'pug')
 
+//Show static files in the public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/static', express.static('public'));
+//Show static in the images folder
+app.use(express.static(path.join(__dirname, '/images/')));
 
+//Sets the index route to render to the home page w/ data from projects.
 app.get('/', function (req, res) {
   res.render('index', {projects: data.projects})
 })
 
-// console.log(data);
-
+//Set the about route to render to the about page.
 app.get('/about', function (req, res) {
   res.render('about')
 })
 
-// app.get('/index', function (req, res) {
-//   res.render('index', {projects: data.projects});
-// })
-
-
+//Set dynamic routes for projects based on their id an render a customized version of each project.
 app.get('/project/:id', function (req, res, next) {
   res.render('project', 
    {
@@ -33,7 +33,26 @@ app.get('/project/:id', function (req, res, next) {
     technologies: data.projects [req.params.id].technologies,
     image_urls : data.projects [req.params.id].imageurl
   });
-}); 
+});
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.use('/imagess', express.static(path.join(__dirname, '/images/')));
+
+
+
+// console.log(data);
+
+app.get('/index', function (req, res) {
+  res.render('index', {projects: data.projects});
+})
+
+app.get('/project', function(req, res) {
+  let projectIndex = req.query.id;
+  res.render('project', {project: data.projects[projectIndex]});
+});
+
+ 
 
 
 app.listen(3000, ()=>{
